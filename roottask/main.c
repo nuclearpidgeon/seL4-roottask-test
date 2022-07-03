e5831638e4f0fb0234534e7005b5f6a4eec739f4
Stewart Webb <stewart.james.webb@gmail.com>
2022-07-04 00:52:30 +1000

    Update comments to better explain initialisation process

--- roottask/main.c
+++ roottask/main.c
@@ -33,7 +33,7 @@ allocman_t *allocman;
 /* static memory for the allocator to bootstrap with */
 #define ALLOCATOR_STATIC_POOL_SIZE ((1 << seL4_PageBits) * 20)
 
-static char allocator_mem_pool[ALLOCATOR_STATIC_POOL_SIZE];
+static char allocator_static_mem_pool[ALLOCATOR_STATIC_POOL_SIZE];
 
 /* static memory for virtual memory bootstrapping */
 static sel4utils_alloc_data_t vspace_alloc_data;
@@ -51,12 +51,13 @@ int main(void)
     seL4_DebugNameThread(seL4_CapInitThreadTCB, "sel4-roottask-test");
 #endif
 
-    /* initialise libsel4simple, which abstracts away which kernel version
-     * we are running on */
+    /* initialise libsel4simple */
     simple_default_init_bootinfo(&simple, binfo);
 
-    /* create an allocator */
-    allocman = bootstrap_use_current_simple(&simple, ALLOCATOR_STATIC_POOL_SIZE, allocator_mem_pool);
+    /* create an allocman allocator, bootstrapped with the current state
+       of system memory from simple, and with the static buffer as the initial
+       area of memory to use for its bookkeeping datastructure storage */
+    allocman = bootstrap_use_current_simple(&simple, ALLOCATOR_STATIC_POOL_SIZE, allocator_static_mem_pool);
     if (allocman == NULL) {
         ZF_LOGF("Failed to create allocman");
     }
@@ -65,7 +66,8 @@ int main(void)
     allocman_make_vka(&vka, allocman);
 
     /* create a vspace (virtual memory management interface). Bootinfo is
-       required so that it will be masked out as already mapped in */
+       required only so that it will be masked out as already mapped into
+       the vspace */
     error = sel4utils_bootstrap_vspace_with_bootinfo_leaky(&vspace,
                                                            &vspace_alloc_data, simple_get_pd(&simple),
                                                            &vka, binfo);
@@ -73,7 +75,9 @@ int main(void)
         ZF_LOGF("Failed to bootstrap vspace");
     }
 
-    /* fill the allocator with virtual memory */
+    /* Reserve a chunk of the vspace for the allocator for use for its own
+       bookkeeping storage beyond the small space of the static pool set up
+       above */
     void *allocator_vpool_vaddr;
     reservation_t virtual_reservation = vspace_reserve_range(&vspace,
                                                ALLOCATOR_VIRTUAL_POOL_SIZE, seL4_AllRights, 1, &allocator_vpool_vaddr);
@@ -84,10 +88,17 @@ int main(void)
     bootstrap_configure_virtual_pool(allocman, allocator_vpool_vaddr,
                                      ALLOCATOR_VIRTUAL_POOL_SIZE, simple_get_pd(&simple));
 
-    // sel4platsupport_new_io_ops
+    // sel4platsupport_new_io_ops?
+
+    /* Now that we have a VSpace and a functioning allocator, setup the
+       platform's serial device/driver. This will switch printf over to
+       writing out to serial directly via talking to the serial device's
+       registers as mapped into the current address space, rather than relying
+       on the seL4_DebugPutChar syscall to do this from kernel space */
     platsupport_serial_setup_simple(&vspace, &simple, &vka);
 
     printf("Serial device is now set up.\n");
+
     printf("OK byeee!\n");
     // 'Quit' by suspending the root thread
     seL4_TCB_Suspend(seL4_CapInitThreadTCB);
